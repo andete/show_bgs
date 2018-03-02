9ce4b2718faa7057c2c01dcc541d5d06e2d55abe
Joost Yervante Damad <joost@damad.be>
2018-03-02 08:42:02 +0100

    sync

--- src/ebgsv4.rs
+++ src/ebgsv4.rs
@@ -1,8 +1,8 @@
 use chrono::{DateTime,Utc};
 
 #[derive(Debug,Deserialize)]
-pub struct EBGSFactionsPageV4 {
-    pub docs:Vec<EBGSFactionsV4>,
+pub struct EBGSPageV4<T> {
+    pub docs:Vec<T>,
     pub page:i64,
     pub pages:i64,
     pub total:i64,
@@ -25,7 +25,7 @@ pub struct EBGSFactionsV4 {
 #[derive(Debug,Deserialize)]
 pub struct EBGSFactionPresenceV4 {
     pub system_name:String,
-    pub state:String,
+    pub state:String, // TODO: Enum
     pub pending_states:Vec<EBGSStateV4>,
     pub recovering_states:Vec<EBGSStateV4>,
     pub influence: f64,
@@ -34,14 +34,14 @@ pub struct EBGSFactionPresenceV4 {
 
 #[derive(Debug,Deserialize)]
 pub struct EBGSStateV4 {
-    pub state:String,
+    pub state:String, // TODO: enum
     pub trend:i64,
 }
 
 #[derive(Debug,Deserialize)]
 pub struct EBGSFactionHistoryV4 {
     pub system:String,
-    pub state:String,
+    pub state:String, // TODO: enum
     pub updated_at:DateTime<Utc>,
     pub system_lower:String,
     pub updated_by:String,
@@ -49,4 +49,44 @@ pub struct EBGSFactionHistoryV4 {
     pub recovering_states:Vec<EBGSStateV4>,
     pub _id:String,
     pub influence:f64,
+}
+
+#[derive(Debug,Deserialize)]
+pub struct EBGSSystemsV4 {
+    pub eddb_id: i64,
+    pub name_lower: String,
+    pub allegiance: String, // TODO: enum
+    pub _id: String,
+    pub population: i64,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+    pub updated_at: DateTime<Utc>,
+    pub state: String, // TODO: enum
+    pub security: String, // TODO: enum
+    pub controlling_minor_faction: String,
+    pub primary_economy: String, // TODO: enum
+    pub name: String,
+    pub government: String, // TODO: enum
+    pub factions: Vec<EBGSSystemPresenceV4>,
+    pub history: Vec<EBGSSystemHistoryV4>,
+}
+
+#[derive(Debug,Deserialize)]
+pub struct EBGSSystemPresenceV4 {
+    pub name: String,
+    pub name_lower: String,
+}
+
+#[derive(Debug,Deserialize)]
+pub struct EBGSSystemHistoryV4 {
+    pub controlling_minor_faction: String,
+    pub security: String, // TODO: enum
+    pub updated_at: DateTime<Utc>,
+    pub state: String, // TODO: enum
+    pub government: String, // TODO: enum
+    pub population: i64,
+    pub updated_by: String,
+    pub allegiance: String, // TODO: enum
+    pub factions: Vec<EBGSSystemPresenceV4>,
 }
--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,20 @@ extern crate serde_json;
 use std::fs::File;
 use std::io::Read;
 
+/*
+
+For each system we're interested in, request the faction list for the last 7 days.
+
+For each unique faction request the last 7 days data.
+
+
+*/
+
 fn main() {
     let mut f = File::open("../faction_oom.json").unwrap();
     let mut contents = String::new();
     f.read_to_string(&mut contents).unwrap();
-    let r:ebgsv4::EBGSFactionsPageV4 = serde_json::from_str(&contents).unwrap();
+    let r:ebgsv4::EBGSPageV4<ebgsv4::EBGSFactionsV4> = serde_json::from_str(&contents).unwrap();
     let r = &r.docs[0];
     for h in &r.history {
         if h.system == "Maausk" {
