41af0760fb9f38833bb1dac834306b259b592e79
Joost Yervante Damad <joost@damad.be>
2018-10-21 15:41:38 +0200

    scoping improvements and formatting fixes

--- src/calculate.rs
+++ src/calculate.rs
@@ -1,9 +1,9 @@
 // (c) 2018 Joost Yervante Damad <joost@damad.be>
 
-use chrono::{Date,Utc};
+use chrono::{Date, Utc};
 use serde_json;
 
-use std::collections::{BTreeSet,HashMap};
+use std::collections::{BTreeSet, HashMap};
 use std::fs::File;
 
 use data;
@@ -11,25 +11,25 @@ use webdata::*;
 use data::Government;
 use Config;
 
-pub fn calculate(config:&Config, yesterday:bool) {
+pub fn calculate(config: &Config, yesterday: bool) {
     info!("Calculating data");
     let mut system_warnings = HashMap::new();
-    let system_names = config.systems();
-    info!("systems to handle: {:?}", system_names);
+    let wanted_system_names = config.systems();
+    info!("systems to handle: {:?}", wanted_system_names);
     let datadir = config.datadir();
     let mut systems = HashMap::new();
     let mut dates = BTreeSet::new();
     let mut system_dates = Vec::new();
-    for system_name in &system_names {
+    for system_name in &wanted_system_names {
         info!("Looking at {}...", system_name);
         let n = format!("{}/systems/{}.json", datadir, system_name);
         let f = File::open(&n).unwrap();
-        let s:data::System = serde_json::from_reader(&f).unwrap();
+        let s: data::System = serde_json::from_reader(&f).unwrap();
         if let Some(d) = s.latest_day() {
             dates.insert(d);
             system_dates.push((system_name, d));
         }
-        let system:System = s.into();
+        let system: System = s.into();
         systems.insert(system_name.clone(), system);
     }
     // info!("systems: {:?}", systems);
@@ -41,7 +41,7 @@ pub fn calculate(config:&Config, yesterday:bool) {
         dates.iter().max().unwrap().pred()
     };
     info!("Current BGS day: {}", bgs_day);
-    for &(system,date) in &system_dates {
+    for &(system, date) in &system_dates {
         if bgs_day != date {
             warn!("System is not up to date: {}: {} {}", system, bgs_day, date);
             //warnings.push(format!("System is not up to date: {}", system));
@@ -49,21 +49,29 @@ pub fn calculate(config:&Config, yesterday:bool) {
     }
 
     let mut global_factions = HashMap::new();
-    let n = format!("{}/minor_factions.json", datadir);
-    let f = File::open(&n).unwrap();
-    let minor_faction_names:BTreeSet<String> = serde_json::from_reader(&f).unwrap();
+
+    let minor_faction_names: BTreeSet<String> = {
+        let n = format!("{}/minor_factions.json", datadir);
+        let f = File::open(&n).unwrap();
+        serde_json::from_reader(&f).unwrap()
+    };
+
     for minor_faction_name in minor_faction_names {
         info!("Looking at {}...", minor_faction_name);
-        let n = format!("{}/factions/{}.json", datadir, minor_faction_name);
-        let f = File::open(&n).unwrap();
-        let data_faction:data::Faction = serde_json::from_reader(&f).unwrap();
+        let data_faction: data::Faction = {
+            let n = format!("{}/factions/{}.json", datadir, minor_faction_name);
+            let f = File::open(&n).unwrap();
+            serde_json::from_reader(&f).unwrap()
+        };
         if data_faction.government == Government::Engineer {
-            continue
+            continue;
         }
-        let fgs:FactionGlobalState = (&data_faction).into();
+
+        let fgs: FactionGlobalState = (&data_faction).into();
         global_factions.insert(minor_faction_name.clone(), fgs);
+
         for system in data_faction.systems() {
-            if !system_names.contains(&system) {
+            if !wanted_system_names.contains(&system) {
                 continue
             }
             if data_faction.latest_day(&system) != bgs_day {
@@ -72,7 +80,7 @@ pub fn calculate(config:&Config, yesterday:bool) {
                 v.push(format!("Faction {} is not up to date in {}", minor_faction_name, system));
             }
         }
-        let faction_template:Faction = (&data_faction).into();
+        let faction_template: Faction = (&data_faction).into();
         for history in data_faction.dynamic.history {
             let mut at_home = false;
             // could be that the system is not in our system list...
@@ -87,8 +95,8 @@ pub fn calculate(config:&Config, yesterday:bool) {
                 if system.controlling.to_lowercase() == faction_template.name.to_lowercase() {
                     faction.controlling = true;
                 }
-                let inf = history.presence.influence*100.0;
-                let mut data:FactionData = history.into();
+                let inf = history.presence.influence * 100.0;
+                let mut data: FactionData = history.into();
                 if !at_home && inf < 2.5 {
                     data.influence_danger = true;
                 }
@@ -99,9 +107,9 @@ pub fn calculate(config:&Config, yesterday:bool) {
             }
         }
     }
-    let mut faction_colors:HashMap<String,String> = HashMap::new();
+    let mut faction_colors: HashMap<String, String> = HashMap::new();
     faction_colors.insert("The Order of Mobius".into(), "black".into());
-    
+
     let mut dates = BTreeSet::new();
     for system in systems.values() {
         for faction in system.factions.values() {
@@ -110,7 +118,7 @@ pub fn calculate(config:&Config, yesterday:bool) {
             }
         }
     }
-    let dates_vec:Vec<Date<Utc>> = dates.iter().cloned().collect();
+    let dates_vec: Vec<Date<Utc>> = dates.iter().cloned().collect();
 
     for system in &mut systems.values_mut() {
         for faction in &mut system.factions.values_mut() {
@@ -134,13 +142,13 @@ pub fn calculate(config:&Config, yesterday:bool) {
             }
             */
         }
-        let mut colors:BTreeSet<String> = vec!["blue", "green", "cyan", "orange",
-                                               "pink", "grey", "magenta", "yellow", "red"].into_iter().map(|x| x.into()).collect();
+        let mut colors: BTreeSet<String> = vec!["blue", "green", "cyan", "orange",
+                                                "pink", "grey", "magenta", "yellow", "red"].into_iter().map(|x| x.into()).collect();
         // first fill in using registered colors, but only if no duplicates
         for faction in &mut system.factions.values_mut() {
             if faction.name == "The Order of Mobius" {
                 faction.color = "black".into();
-                continue
+                continue;
             }
             if let Some(color) = faction_colors.get(&faction.name) {
                 //println!("Found existing color: {} for {}", color, faction.name);
@@ -164,7 +172,7 @@ pub fn calculate(config:&Config, yesterday:bool) {
             system.factions_by_inf.push(faction.clone())
         }
         system.factions_by_inf.sort_by(
-            |a,b| b.latest_inf().cmp(&a.latest_inf())
+            |a, b| b.latest_inf().cmp(&a.latest_inf())
         );
     }
 
@@ -172,24 +180,24 @@ pub fn calculate(config:&Config, yesterday:bool) {
     let f = File::create(&n).unwrap();
 
     // order by order in Config
-    let mut s2:Vec<System> = vec![];
-    for name in &system_names {
+    let mut s2: Vec<System> = vec![];
+    for name in &wanted_system_names {
         let mut system = systems.remove(name).unwrap();
         system.warnings = system_warnings.remove(name).unwrap_or(vec![]);
         s2.push(system)
     }
 
-    let dates:Vec<String> = dates_vec.iter().skip(1).map(|e| format!("{}", e.format("%d/%m"))).collect();
+    let dates: Vec<String> = dates_vec.iter().skip(1).map(|e| format!("{}", e.format("%d/%m"))).collect();
     let dates2 = dates.clone();
     let dates10 = dates.as_slice();
     let dates10 = dates10.windows(10).last().unwrap().to_vec();
     let systems = Systems {
-        report_name:config.report_name.clone(),
+        report_name: config.report_name.clone(),
         systems: s2,
-        dates:dates2,
-        dates10:dates10,
-        bgs_day:format!("{}", bgs_day.format("%d/%m")),
-        factions:global_factions,
+        dates: dates2,
+        dates10: dates10,
+        bgs_day: format!("{}", bgs_day.format("%d/%m")),
+        factions: global_factions,
     };
     serde_json::to_writer_pretty(&f, &systems).unwrap();
 }
